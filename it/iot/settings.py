a5bb3cfc2961e597e1a7d26dde80a7befefdea5c
root <root@emb-ban-089.embitel.local>
2015-10-16 14:09:13 +0530

    IOT Init code

--- it/iot/settings.py
+++ it/iot/settings.py
@@ -1,3 +1,4 @@
+ot
 """
 Django settings for iot project.
 
@@ -127,8 +128,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'iot', # Or path to database file if using sqlite3.
-        'USER': 'iot', # Not used with sqlite3.
-        'PASSWORD': 'iot',#'84adf87feae4a6f0c63f0a3df7d6c456', # Not used with sqlite3.
+        'USER': '', # Not used with sqlite3.
+        'PASSWORD': '',#'84adf87feae4a6f0c63f0a3df7d6c456', # Not used with sqlite3.
         'HOST': '127.0.0.1', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '', # Set to empty string for default. Not used with sqlite3.
     },
